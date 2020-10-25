535340a2decc5390cb996b4de42579a15539e5b1
yamawo <y7a3m1asaki@gmail.com>
2020-10-25 17:23:24 +0900

    delete redundant code

--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+interface State {
+  isSpinner: boolean
 }
 
-export default MyApp;
+export default class extends App<Record<string, unknown>, Record<string, unknown>, State>
