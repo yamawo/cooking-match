c2818e90a218885dcd2c954cd596f02b905a45c9
yamawo <y7a3m1asaki@gmail.com>
2020-10-25 18:45:34 +0900

    wip

--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -1,7 +1,8 @@
+import App from "next/App";
 import "../styles/globals.css";
 
 interface State {
-  isSpinner: boolean
+  isSpinner: boolean;
 }
 
-export default class extends App<Record<string, unknown>, Record<string, unknown>, State>
+export default class extends App<Record<string, unknown>, Record<string, unknown>, State> {}
